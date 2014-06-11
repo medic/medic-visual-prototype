56efacc3936259fd3d10d9329358426a9bfe1c81
Gareth Bowen <gareth@bowenwebdesign.co.nz>
2014-06-11 16:15:39 +1200

    Added responsive styling. Added css transitions for item selection.

---
+++ static/scripts/kujua-visual.js
@@ -0,0 +1,16 @@
+$(function() {
+  $('.inbox-items').on('click', 'li', function(e) {
+    e.preventDefault();
+    var elem = $(this);
+    if (!elem.is('selected')) {
+      var inboxItems = $('.inbox-items');
+      inboxItems.find('.selected').removeClass('selected');
+      elem.addClass('selected');
+      $('.row.content').addClass('show-body');
+    }
+  });
+  $('.slide-back').on('click', function(e) {
+    e.preventDefault();
+    $('.row.content').removeClass('show-body');
+  });
+});
